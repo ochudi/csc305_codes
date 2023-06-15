0ad6f824af0c30c25cbee90b0245c34de4c314fb
ochudi <chukwudi.ofoma@pau.edu.ng>
2023-06-15 18:02:26 +0100

    Include class codes

--- os_with_bootloader/kernel_with_bootloader/src/main.rs
+++ os_with_bootloader/kernel_with_bootloader/src/main.rs
@@ -1,7 +1,15 @@
 #![no_std]
 #![no_main]
 
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {
+        hlt();
+    }
+}
+
 use bootloader_api::config::Mapping;
+use writer::FrameBufferWriter;
 use x86_64::instructions::hlt;
 
 //Use the entry_point macro to register the entry point function: bootloader_api::entry_point!(kernel_main)
@@ -18,18 +26,27 @@ pub static BOOTLOADER_CONFIG: bootloader_api::BootloaderConfig = {
     config
 };
 
+use core::fmt::{Arguments, Write};
+mod writer;
+
 bootloader_api::entry_point!(my_entry_point, config = &BOOTLOADER_CONFIG);
 
 fn my_entry_point(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    //boot_info.framebuffer is our target for display
+    let frame_buffer_info = boot_info.framebuffer.as_mut().unwrap().info();
+    let buffer = boot_info.framebuffer.as_mut().unwrap().buffer_mut();
+
+    let mut frame_buffer_writer = FrameBufferWriter::new(buffer, frame_buffer_info);
+
+    write!(
+        frame_buffer_writer,
+        "Testing testing {} and {}",
+        1,
+        4.0 / 2.0
+    )
+    .unwrap();
+
     loop {
         hlt(); //stop x86_64 from being unnecessarily busy while looping
-    }
-}
-
-#[panic_handler]
-
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {
-        hlt();
     }
 }
