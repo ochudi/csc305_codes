af6f685e48dc78d77458d3b7398106525c039c05
ochudi <chukwudi.ofoma@pau.edu.ng>
2023-06-30 08:40:06 +0100

    Include a free-standing Rust binary

---
+++ os/src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    //panic!("Some panic message");
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
